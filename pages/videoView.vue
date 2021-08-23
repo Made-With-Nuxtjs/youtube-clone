1b217056f3d9ac807e663445e2fbd73af69c2157
ICTNirob <71205100+ICTNirob@users.noreply.github.com>
2021-08-23 12:34:15 +0600

    Update videoView.vue

--- pages/videoView.vue
+++ pages/videoView.vue
@@ -6,31 +6,77 @@
     >
       <!-- style="flex-wrap: nowrap;" -->
       <v-col
-        cols="8"
+        md="8" lg="8" sm="12" xs="12"
         style="min-width: 100px; max-width: 100%;"
         class="flex-grow-1 flex-shrink-0"
       >
-        <v-card
-          class="pa-2"
-          outlined
-          tile
-        >
-          I'm 1 column wide and I grow to take all the space
-          I'm 1 column wide and I grow to take all the space
-          I'm 1 column wide and I grow to take all the space
-         
-        </v-card>
+          <!-- <v-card
+             class="mx-auto" 
+          > -->
+          <v-responsive :aspect-ratio="16/9">
+            <v-container fluid>
+              <v-row dense>
+                <v-col
+                  v-for="card in cards"
+                  :key="card.title"
+                  :cols="card.flex"
+                >
+                  <v-card>
+                    <v-img
+                      :src="card.src"
+                      gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+                      height="600px"
+                    >
+                    </v-img>
+
+                    <v-card-actions>
+                      <v-btn icon>
+                        <v-icon>mdi-heart</v-icon>
+                      </v-btn>
+
+                      <v-btn icon>
+                        <v-icon>mdi-bookmark</v-icon>
+                      </v-btn>
+
+                      <v-btn icon>
+                        <v-icon>mdi-share-variant</v-icon>
+                      </v-btn>
+                      <v-spacer></v-spacer>
+
+                      <v-btn icon>
+                        <v-icon>mdi-heart</v-icon>
+                      </v-btn>
+
+                      <v-btn icon>
+                        <v-icon>mdi-bookmark</v-icon>
+                      </v-btn>
+
+                      <v-btn icon>
+                        <v-icon>mdi-share-variant</v-icon>
+                      </v-btn>
+                    </v-card-actions>
+                  </v-card>
+                </v-col>
+              </v-row>
+            </v-container>
+            </v-responsive>
+          <!-- </v-card> -->
+
+
+
       </v-col>
       <v-col
-        cols="4"
+      md="4" lg="4" sm="12" xs="12"
         style="min-width: 100px;"
         class="flex-grow-0 flex-shrink-1"
       >
+        <CategoryChips/>
         <v-card
-          class="pa-2"
+          class="pa-1"
           outlined
           tile
         >
+
           I'm 5 column wide and I shrink if there's not enough space I'm 1 column wide and I grow to take
            all the space I'm 1 column wide and I grow to take all the space I'm 1 column wide and I grow to take all the space
         </v-card>
@@ -38,6 +84,18 @@
     </v-row>
   </v-container>
 </template>
+
+
+
+<script>
+  export default {
+    data: () => ({
+      cards: [
+        { title: 'Pre-fab homes', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', flex: 12 },
+      ],
+    }),
+  }
+</script>
 
     
 
